fbcac9a8b2737b316627502ed9ca9b4e9d5fa804
Daniel Kim <118289602+danyobosayo@users.noreply.github.com>
2024-10-27 00:38:40 -0500

    Feat: Initial Screen Implementation

--- app/index.tsx
+++ app/index.tsx
@@ -2,14 +2,16 @@ import { Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Home from "./tabs/Home";
+import Settings from "./tabs/Settings";
+import { RootStackParamList } from "@/types";
 
-
-const Stack = createNativeStackNavigator();
+const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export default function Index() {
   return (
       <Stack.Navigator screenOptions={{ headerShown: false }}>
         <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Settings" component={Settings} />
       </Stack.Navigator>
   );
 }
---
+++ app/tabs/Settings.tsx
@@ -0,0 +1,132 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { Car, MessageCircleMore, Blocks, ChevronLeft } from "lucide-react";
+
+const Settings = () => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.margin}>
+        <View>
+          <Text style={styles.button}> <ChevronLeft style={styles.arrow}/> </Text>
+          <Text style={styles.title}>Choose your output style!</Text>
+          <Text style={styles.subheading}>Required to pick one</Text>
+        </View>
+        <View>
+          <View style={[styles.selection, styles.verticalContainer]}>
+            <Blocks style={styles.icon} />
+            <Text style={styles.buttonText}>Colored Shapes</Text>
+          </View>
+          <View style={[styles.selection, styles.verticalContainer]}>
+            <MessageCircleMore style={styles.icon} />
+            <Text style={styles.buttonText}>Text</Text>
+          </View>
+          <View style={[styles.selection, styles.verticalContainer]}>
+            <Car style={styles.icon} />
+            <Text style={styles.buttonText}>Both</Text>
+          </View>
+        </View>
+        {/* <Text style={styles.bottomBar}> -- </Text> */}
+      </View>
+      <View style={styles.bottomBar}>
+        <Text style={styles.startButton}>Start Driving</Text>
+      </View>
+    </View>
+  );
+};
+
+export default Settings;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  verticalContainer: {
+    flex: 1,
+    flexDirection: "column",
+  },
+  margin: {
+    margin: 20,
+    flex: 1,
+  },
+  title: {
+    flex: 1,
+    fontSize: 24,
+    alignItems: "center",
+    alignSelf: "flex-start",
+    paddingTop: 50,
+    fontWeight: "bold",
+  },
+  button: {
+    flex: 1,
+    top: 40,
+    maxWidth: 48,
+    minHeight: 36,
+    minWidth: 48,
+    maxHeight: 36,
+    borderRadius: 8,
+    paddingTop:6,
+    justifyContent: "center",
+    textAlign: "center",
+    alignItems: "center",
+    backgroundColor: "#DEDCFF",
+  },
+  subheading: {
+    flex: 1,
+    color: "gray",
+    fontSize: 14,
+    fontWeight: "bold",
+    alignItems: "center",
+    alignSelf: "flex-start",
+    paddingTop: 5,
+    paddingBottom: 25,
+  },
+  selection: {
+    flex: 1,
+    backgroundColor: "#DEDCFF",
+    borderRadius: 12,
+    paddingTop: 25,
+    paddingBottom: 25,
+    paddingLeft: 15,
+    alignSelf: "stretch",
+    justifyContent: "center",
+    textAlign: "center",
+    alignItems: "center",
+ 
+    marginVertical: 20,
+  },
+  bottomBar: {
+    backgroundColor: "#DEDCFF",
+    height: 120,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  startButton: {
+    flex: 1,
+    alignItems: "center",
+    alignSelf: "center",
+    justifyContent: "center",
+    borderRadius: 15,
+    color: "white",
+    fontSize: 24,
+    fontWeight: "bold",
+    paddingHorizontal: 50,
+    paddingVertical: 15,
+    backgroundColor: "#433BFF",
+    marginVertical: 30,
+  },
+  icon: {
+    backgroundColor: "transparent",
+    color: "black",
+    width: 48,
+    height: 48,
+  },
+  arrow: {
+    backgroundColor: "transparent",
+    color: "black",
+  },
+  buttonText:{
+    fontSize: 20,
+    color: "black",
+    fontWeight: "bold",
+  }
+});
