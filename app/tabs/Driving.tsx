35cafacd45b2202269cfb9fa1d359cfb63828713
duyenemily <duyenemily.nguyen@gmail.com>
2024-10-27 04:13:19 -0500

    Added styling and output box to Driving component

--- app/tabs/Driving.tsx
+++ app/tabs/Driving.tsx
@@ -1,18 +1,34 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Pressable, StyleSheet, Text, View } from "react-native";
 import React from "react";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { RootStackParamList } from "@/types";
 
 type DrivingProps = NativeStackScreenProps<RootStackParamList, "Driving">;
 
-const Driving = ({ route }: DrivingProps) => {
+const Driving = ({ route, navigation }: DrivingProps) => {
   const { outputStyle } = route.params;
+
+  const getOutput = () => {
+    // shapes, text, both
+    return outputStyle;
+  };
 
   return (
     <View style={styles.container}>
-      <Text>Driving Screen Here</Text>
-      <Text>{outputStyle}</Text>
-        
+      <View style={styles.margin}>
+        <Pressable
+          style={({ pressed }) => [
+            styles.button,
+            pressed && styles.buttonPressed,
+          ]}
+          onPress={() => navigation.navigate("Home")}
+        >
+          <Text style={styles.buttonText}>End Drive</Text>
+        </Pressable>
+        <View style={[styles.verticalContainer, styles.output]}>
+          <Text>{getOutput()}</Text>
+        </View>
+      </View>
     </View>
   );
 };
@@ -23,8 +39,44 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
+  margin: {
+    flex: 1,
+    marginHorizontal: 20,
+    marginTop: 50,
+    marginBottom: 20,
+  },
   verticalContainer: {
     flex: 1,
     flexDirection: "column",
+  },
+  button: {
+    backgroundColor: "#433BFF",
+    borderRadius: 15,
+    paddingHorizontal: 50,
+    paddingVertical: 15,
+    marginVertical: 10,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  buttonPressed: {
+    opacity: 0.5,
+  },
+  buttonText: {
+    color: "white",
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+  output: {
+    flex: 1,
+    backgroundColor: "#FFFFFF",
+    borderWidth: 12,
+    borderColor: "#E9E9EC",
+    borderRadius: 12,
+    paddingVertical: 25,
+    paddingHorizontal: 15,
+    marginVertical: 20,
+    maxHeight: 475,
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
